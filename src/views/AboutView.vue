6c6bc31c142e9f8effd019996aaaa73ee4613db9
xiaoxian-xiaohua <2517026539@qq.com>
2023-11-10 17:09:13 +0800

    fix：

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,27 @@
+<script setup>
+  import { ref, reactive } from 'vue';
+  const count = ref(0)
+  const obj = ref({ a: 1 })
+  const obj1 = reactive({ a: 1 })
+  const rawHtml = ref("<span style='color: red;'>This should be red.</span>")
+  const row = {}
+  const proxy = reactive(row)
+  console.log(row === proxy);
+  console.log(obj.value)
+  console.log(obj1)
+  function reduce() {
+    count.value--
+  }
+  function add() {
+    count.value++
+  }
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>计数：<button @click="reduce">-</button>{{ count }}<button @click="add">+</button></div>
+    <p v-html="rawHtml"></p>
+    <p>{{ rawHtml }}</p>
   </div>
 </template>
 
@@ -10,6 +31,8 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
   }
 }
 </style>
